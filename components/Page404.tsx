043023885cc0cec1eb6a7e5c6c229c23c9021fc6
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-08 22:31:18 +1200

    feat(404): :lipstick: update `404` page

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -1,38 +1,26 @@
 import * as React from 'react'
+import cs from 'classnames';
+
 import * as types from 'lib/types'
+import * as config from 'lib/config';
+
 import { PageHead } from './PageHead'
 
 import styles from './styles.module.css'
 
-export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
-  const title = site?.name || 'Notion Page Not Found'
+export const Page404: React.FC<types.PageProps> = ({ site }) => {
+  const title = site?.name || `Page Not Found | ${config.name}`
 
   return (
     <>
       <PageHead site={site} title={title} />
 
-      <div className={styles.container}>
+      <a className={cs(styles.container, styles.error404)} href='/'>
         <main className={styles.main}>
-          <h1>Notion Page Not Found</h1>
-
-          {error ? (
-            <p>{error.message}</p>
-          ) : (
-            pageId && (
-              <p>
-                Make sure that Notion page &quot;{pageId}&quot; is publicly
-                accessible.
-              </p>
-            )
-          )}
-
-          <img
-            src='/404.png'
-            alt='404 Not Found'
-            className={styles.errorImage}
-          />
+          <h1>I can&apos;t seem to find that page.</h1>
+          <h2>Click anywhere to go home.</h2>
         </main>
-      </div>
+      </a>
     </>
   )
 }
