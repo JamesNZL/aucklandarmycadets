467d2dcfec0c4deb7fb4c41429f0b27fd386d723
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-08 16:22:41 +1200

    fix: :rotating_light: remove unused variable

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,12 +11,6 @@ import styles from './styles.module.css'
 // TODO: merge the data and icons from PageSocial with the social links in Footer
 
 export const FooterImpl: React.FC = () => {
-  const [hasMounted, setHasMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setHasMounted(true)
-  }, [])
-
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright 2022 {config.author}</div>
