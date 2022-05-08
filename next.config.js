7e260352f2e9a5c4b5bb5e02720c0715cd573421
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-08 19:19:16 +1200

    refactor: :wrench: remove transitivebs domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
-})
+});
 
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
@@ -12,11 +12,10 @@ module.exports = withBundleAnalyzer({
       'images.unsplash.com',
       'pbs.twimg.com',
       'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
+      's3.us-west-2.amazonaws.com'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
-})
+});
