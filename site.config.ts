5b246e6b51defd07f0a329affeba1ccd38cf10a0
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-08 14:28:14 +1200

    feat: :construction: point to temporary landing page on `prod`

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd9e4d2d799734db09a547021f200d5bc',
+  rootNotionPageId: '45e6846742494406bfd12b41fead55f9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
