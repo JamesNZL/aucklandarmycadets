080b969607caf1e6690162ea9a590f7188f1f16a
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-12 09:58:41 +1200

    feat: :mag: make `www` the canonical form

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'City of Auckland Cadet Unit',
-  domain: 'aucklandarmycadets.com',
+  domain: 'www.aucklandarmycadets.com',
   author: 'City of Auckland Cadet Unit',
 
   // open graph metadata (optional)
