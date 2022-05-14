efe9f5a90ebb2abcfbd268dc3f5a880da4dd9a4e
James <44986932+JamesNZL@users.noreply.github.com>
2022-05-14 20:21:21 +1200

    fix: :bug: fix improper implementation in a3f17b2

--- lib/get-site-map.ts
+++ lib/get-site-map.ts
@@ -30,12 +30,8 @@ async function getAllPagesImpl(
   rootNotionSpaceId: string
 ): Promise<Partial<types.SiteMap>> {
   const getPage = async (pageId: string, ...args) => {
-    pageId = uuidToId(pageId)
-
-    if (config.exposedRouteIds.length && !config.exposedRouteIds.includes(pageId)) return null
-    else if (pageId === config.rootNotionPageId) return null;
-
-    console.log('\nnotion getPage', pageId)
+    if (config.exposedRouteIds.length && !config.exposedRouteIds.includes(uuidToId(pageId))) return null
+    console.log('\nnotion getPage', uuidToId(pageId))
     return notion.getPage(pageId, ...args)
   }
 
@@ -55,6 +51,8 @@ async function getAllPagesImpl(
       const canonicalPageId = getCanonicalPageId(pageId, recordMap, {
         uuid
       })
+
+      if (uuidToId(pageId) === config.rootNotionPageId) return map
 
       if (map[canonicalPageId]) {
         // you can have multiple pages in different collections that have the same id
