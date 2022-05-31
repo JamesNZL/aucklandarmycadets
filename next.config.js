132430547a8284274cac2f75f671d9ad80bce1de
James <james@jamesnzl.xyz>
2022-05-31 19:12:14 +1200

    chore: :wrench: broaden allowed domain

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'cdn.aucklandarmycadets.org.nz'
+      'aucklandarmycadets.org.nz'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
