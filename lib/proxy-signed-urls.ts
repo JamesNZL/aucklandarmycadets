0def7898008c3e5b5e4287829a4a599aa09c2346
James <james@jamesnzl.xyz>
2022-05-31 20:24:58 +1200

    style: :art: indent with spaces

--- lib/proxy-signed-urls.ts
+++ lib/proxy-signed-urls.ts
@@ -4,29 +4,29 @@ import pMemoize from 'p-memoize'
 import { ExtendedRecordMap } from 'notion-types'
 
 export async function proxySignedUrls(
-	recordMap: ExtendedRecordMap
+  recordMap: ExtendedRecordMap
 ): Promise<void> {
-	const urls: string[] = Object.values(recordMap.signed_urls)
+  const urls: string[] = Object.values(recordMap.signed_urls)
 
-	await pMap(
-		urls,
-		async (url) => {
-			console.log(`Fetching https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
-			try {
-				const res = await memoizedGot(`https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
-				console.log(`Fetched https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
-				if (res.statusCode < 200 || res.statusCode >= 300) console.log(res)
-			} catch (err) {
-				console.warn(err)
-			}
-			return
-		},
-		{
-			concurrency: 8
-		}
-	)
+  await pMap(
+    urls,
+    async (url) => {
+      console.log(`Fetching https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
+      try {
+        const res = await memoizedGot(`https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
+        console.log(`Fetched https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
+        if (res.statusCode < 200 || res.statusCode >= 300) console.log(res)
+      } catch (err) {
+        console.warn(err)
+      }
+      return
+    },
+    {
+      concurrency: 8
+    }
+  )
 
-	return
+  return
 }
 
 const memoizedGot = pMemoize(got)
