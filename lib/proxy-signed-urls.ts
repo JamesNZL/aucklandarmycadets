a65c09c354ed8171e06620db7e99c2c656ae03ee
James <james@jamesnzl.xyz>
2022-05-31 16:10:29 +1200

    feat: :loud_sound: log `got` completion

--- lib/proxy-signed-urls.ts
+++ lib/proxy-signed-urls.ts
@@ -13,6 +13,7 @@ export async function proxySignedUrls(
 		async (url) => {
 			console.log(`Fetching https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
 			const res = await memoizedGot(`https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
+			console.log(`Fetched https://cdn.aucklandarmycadets.org.nz/${encodeURIComponent(url)}`)
 			if (res.statusCode < 200 || res.statusCode >= 300) console.log(res)
 			return
 		},
